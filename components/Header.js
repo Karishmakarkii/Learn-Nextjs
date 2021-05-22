5978253f9936bb05150e7e01801833d10535eab1
Karishma karki <karkikarishma143@gmail.com>
2021-05-22 12:01:55 +0545

    first commit style jsx

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import headerStyles from '../styles/Header.module.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="title">
+                <span>WebDev</span> News
+            </h1>
+            <style jsx>
+                {`
+                .title{
+                    color:red;
+                }
+                `}
+            </style>
+        </div>
+    )
+}
+
+export default Header
---
+++ pages/about.js
@@ -0,0 +1,9 @@
+
+ const about = () => {
+    return (
+        <div>
+            <h1>Hello this is about page</h1>
+        </div>
+    )
+}
+export  default about
