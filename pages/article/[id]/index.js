b30e5512835ec33b4a3714a9f95721f9b96934e9
Karishma karki <karkikarishma143@gmail.com>
2021-05-22 22:04:28 +0545

    use of getserversideprop to create dynamic path

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,13 +1,28 @@
+import Link from'next/link'
 import {useRouter} from 'next/router'
 
-const article = () => {
-    const router = useRouter()
-    const {id} = router.query
+const article = ({article}) => {
+    // const router = useRouter()
+    // const {id} = router.query
     return (
-        <div>
-            This is an article {id}
-        </div>
+        <>
+          <h1>{article.id}</h1>
+            <p>{article.body}</p>
+            <br />
+            <Link href='/'>Go Back</Link>
+        </>
     )
+}
+
+export const getServerSideProps = async (context) =>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`) 
+
+const article = await res.json()
+return{
+   props: {
+       article
+   }
+}
 }
 
 export default article
