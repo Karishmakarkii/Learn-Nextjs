b0344226b34845711c43a4e2103d5aefbe72c2c6
Karishma karki <karkikarishma143@gmail.com>
2021-05-22 22:18:59 +0545

    getstaticprops and getstaticpath to generate dynamic path

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -14,7 +14,17 @@ const article = ({article}) => {
     )
 }
 
-export const getServerSideProps = async (context) =>{
+// export const getServerSideProps = async (context) =>{
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`) 
+
+// const article = await res.json()
+// return{
+//    props: {
+//        article
+//    }
+// }
+// }
+export const getStaticProps = async (context) =>{
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`) 
 
 const article = await res.json()
@@ -22,6 +32,18 @@ return{
    props: {
        article
    }
+}
+}
+export const getStaticPaths = async (context) =>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`) 
+
+const articles = await res.json()
+const ids = articles.map(article=> article.id)
+
+const paths = ids.map(id=>({params: {id: id.toString()}}))
+return{
+    paths,
+    fallback: false //if we to something that doesnot exist in data it will go 404 error
 }
 }
 
